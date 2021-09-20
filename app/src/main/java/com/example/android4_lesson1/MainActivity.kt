d015a2a260cc34fb4ddd10d3fbe753be5adc46fb
MurzilinSin <asair4ik@mail.ru>
2021-09-20 20:15:27 +0300

    mvp cicerone

--- app/src/main/java/com/example/android4_lesson1/MainActivity.kt
+++ app/src/main/java/com/example/android4_lesson1/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.android4_lesson1
 
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import com.example.android4_lesson1.databinding.ActivityMainBinding
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
 
-class MainActivity : AppCompatActivity(),MainView {
+class MainActivity : MvpAppCompatActivity(), MainView {
     private var _bind: ActivityMainBinding? = null
     private val bind get() = _bind!!
-    private val presenter = MainPresenter(this)
+    private val presenter by moxyPresenter {
+       MainPresenter(CountersModel())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/android4_lesson1/MainPresenter.kt
+++ app/src/main/java/com/example/android4_lesson1/MainPresenter.kt
@@ -1,17 +1,19 @@
 package com.example.android4_lesson1
 
-class MainPresenter(private val view: MainView) {
-    private val model = CountersModel()
+import moxy.MvpPresenter
+
+class MainPresenter(private val model: CountersModel): MvpPresenter<MainView>() {
+
     fun counterOneClick() {
         val nextValue = model.next(0)
-        view.setButtonOneText(nextValue.toString())
+        viewState.setButtonOneText(nextValue.toString())
     }
     fun counterTwoClick() {
         val nextValue = model.next(1)
-        view.setButtonTwoText(nextValue.toString())
+        viewState.setButtonTwoText(nextValue.toString())
     }
     fun counterThreeClick() {
         val nextValue = model.next(2)
-        view.setButtonThreeText(nextValue.toString())
+        viewState.setButtonThreeText(nextValue.toString())
     }
 }
